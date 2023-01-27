6827c425b3e05159cc58d002db72488a165dd6d8
JacobJax <jacobjax617@gmail.com>
2023-01-27 17:24:45 +0300

    setup project

---
+++ src/components/destinations.js
@@ -0,0 +1,36 @@
+import mtfuji from '../assets/mtfuji.png'
+import sydney from '../assets/sydney.png'
+import geirange from '../assets/geirange.png'
+
+export default [
+   {
+      id: 0,
+      title: "Mount Fuji",
+      description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+      location: "JAPAN",
+      googlemapslink: "",
+      start_date: "12 Jan, 2021",
+      end_date: "24 Jan, 2021",
+      imageurl: mtfuji
+   },
+   {
+      id: 1,
+      title: "Sydney Opera House",
+      description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+      location: "AUSTRALIA",
+      googlemapslink: "",
+      start_date: "27 May, 2021",
+      end_date: "8 Jun, 2021",
+      imageurl: sydney
+   },
+   {
+      id: 2,
+      title: "Geirangerfjord",
+      description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+      location: "NORWAY",
+      googlemapslink: "",
+      start_date: "01 Oct, 2021",
+      end_date: "18 Nov, 2021",
+      imageurl: geirange
+   },
+]
