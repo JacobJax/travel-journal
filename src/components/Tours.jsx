e0981a1b59a3c8d641dc7aa8182d0b73cafb22ca
JacobJax <jacobjax617@gmail.com>
2023-01-28 10:36:28 +0300

    create card component

---
+++ src/components/Tours.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card"
+import './Tours.css'
+
+function Tours() {
+   return (
+      <section id="tours" className="span--max">
+         <Card />
+      </section>
+   )
+}
+
+export default Tours
